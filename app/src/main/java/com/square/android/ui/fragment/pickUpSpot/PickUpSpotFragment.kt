05198325d84ace9dc706f8e5fc1477e7318a4ead
Psycode Interactive <psycodeinteractive@gmail.com>
2019-07-09 17:53:02 +0200

    WIP Pick up - map removal

--- app/src/main/java/com/square/android/ui/fragment/pickUpSpot/PickUpSpotFragment.kt
+++ app/src/main/java/com/square/android/ui/fragment/pickUpSpot/PickUpSpotFragment.kt
@@ -55,22 +55,22 @@ class PickUpSpotFragment: BaseFragment(), PickUpSpotView, DaysAdapter.Handler{
         super.onViewCreated(view, savedInstanceState)
 
         pickupBook.setOnClickListener { presenter.bookClicked() }
-        pickupDayLeft.setOnClickListener {
-            if(clickEnabled){
-                presenter.changeMonth(2)
-            }
-        }
-        pickupDayRight.setOnClickListener {
-            if(clickEnabled){
-                presenter.changeMonth(1)
-            }
-        }
-
-        pickupAddressCl.setOnClickListener {
-            if(clickEnabled){
-                presenter.addressClicked()
-            }
-        }
+//        pickupDayLeft.setOnClickListener {
+//            if(clickEnabled){
+//                presenter.changeMonth(2)
+//            }
+//        }
+//        pickupDayRight.setOnClickListener {
+//            if(clickEnabled){
+//                presenter.changeMonth(1)
+//            }
+//        }
+//
+//        pickupAddressCl.setOnClickListener {
+//            if(clickEnabled){
+//                presenter.addressClicked()
+//            }
+//        }
     }
 
     override fun itemClicked(position: Int) {
@@ -84,75 +84,75 @@ class PickUpSpotFragment: BaseFragment(), PickUpSpotView, DaysAdapter.Handler{
     }
 
     override fun dataLoaded() {
-        pickupMainProgress.visibility = View.GONE
-        pickupAddressCl.visibility = View.VISIBLE
+//        pickupMainProgress.visibility = View.GONE
+//        pickupAddressCl.visibility = View.VISIBLE
     }
 
     override fun setContentLoading() {
-        pickupContent.visibility = View.GONE
-        pickupContentProgress.visibility = View.VISIBLE
+//        pickupContent.visibility = View.GONE
+//        pickupContentProgress.visibility = View.VISIBLE
     }
 
     override fun setContentNormal() {
-        pickupContentProgress.visibility = View.GONE
-        pickupContent.visibility = View.VISIBLE
+//        pickupContentProgress.visibility = View.GONE
+//        pickupContent.visibility = View.VISIBLE
     }
 
     override fun setSlotsLoading() {
-        pickupBook.isEnabled = false
-
-        pickupSv.visibility = View.GONE
-        pickupEmpty.visibility = View.GONE
-
-        pickupSpotsProgress.visibility = View.VISIBLE
+//        pickupBook.isEnabled = false
+//
+//        pickupSv.visibility = View.GONE
+//        pickupEmpty.visibility = View.GONE
+//
+//        pickupSpotsProgress.visibility = View.VISIBLE
     }
 
     override fun setSlotsNormal() {
-        pickupSpotsProgress.visibility = View.GONE
+//        pickupSpotsProgress.visibility = View.GONE
     }
 
     override fun assignAddress(locationWrapper: CampaignInterval.Location) {
-        pickupAddress.text = locationWrapper.getAddressString()
-        pickupAddressLl.visibility = View.VISIBLE
+//        pickupAddress.text = locationWrapper.getAddressString()
+//        pickupAddressLl.visibility = View.VISIBLE
     }
 
     override fun changeDate(calendar: Calendar, useCalendarDay: Boolean) {
-        val month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault())
-        pickupMonth.text = getString(R.string.calendar_format, month, calendar.get(Calendar.YEAR))
-
-        val days = mutableListOf<Day>()
-        val calendar2 = Calendar.getInstance().apply { timeInMillis = calendar.timeInMillis }
-        calendar2.set(Calendar.DAY_OF_MONTH, 1)
-
-        for (x in 0 until calendar2.getActualMaximum(Calendar.DAY_OF_MONTH)) {
-            val day = Day()
-
-            day.monthNumber = calendar2.get(Calendar.MONTH) + 1
-            day.dayValue = calendar2.get(Calendar.DAY_OF_MONTH)
-            day.dayName = calendar2.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault()).substring(0, 1)
-
-            days.add(day)
-
-            calendar2.add(Calendar.DAY_OF_YEAR, 1)
-        }
-
-        adapter = DaysAdapter(days.toList(), this)
-        pickupCalendar.adapter = adapter
-        pickupCalendar.layoutManager = LinearLayoutManager(pickupCalendar.context, RecyclerView.HORIZONTAL,false)
-
-        if(useCalendarDay){
-            val position = calendar.get(Calendar.DAY_OF_MONTH) - 1
-
-            adapter!!.selectedItemPosition = position
-            adapter!!.notifyItemChanged(position, DaysAdapter.SelectedPayload)
-
-            //TODO check if working
-            pickupCalendar.scrollToPosition(position)
-
-        } else{
-            adapter!!.selectedItemPosition = 0
-            adapter!!.notifyItemChanged(0, DaysAdapter.SelectedPayload)
-        }
+//        val month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault())
+//        pickupMonth.text = getString(R.string.calendar_format, month, calendar.get(Calendar.YEAR))
+//
+//        val days = mutableListOf<Day>()
+//        val calendar2 = Calendar.getInstance().apply { timeInMillis = calendar.timeInMillis }
+//        calendar2.set(Calendar.DAY_OF_MONTH, 1)
+//
+//        for (x in 0 until calendar2.getActualMaximum(Calendar.DAY_OF_MONTH)) {
+//            val day = Day()
+//
+//            day.monthNumber = calendar2.get(Calendar.MONTH) + 1
+//            day.dayValue = calendar2.get(Calendar.DAY_OF_MONTH)
+//            day.dayName = calendar2.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault()).substring(0, 1)
+//
+//            days.add(day)
+//
+//            calendar2.add(Calendar.DAY_OF_YEAR, 1)
+//        }
+//
+//        adapter = DaysAdapter(days.toList(), this)
+//        pickupCalendar.adapter = adapter
+//        pickupCalendar.layoutManager = LinearLayoutManager(pickupCalendar.context, RecyclerView.HORIZONTAL,false)
+//
+//        if(useCalendarDay){
+//            val position = calendar.get(Calendar.DAY_OF_MONTH) - 1
+//
+//            adapter!!.selectedItemPosition = position
+//            adapter!!.notifyItemChanged(position, DaysAdapter.SelectedPayload)
+//
+//            //TODO check if working
+//            pickupCalendar.scrollToPosition(position)
+//
+//        } else{
+//            adapter!!.selectedItemPosition = 0
+//            adapter!!.notifyItemChanged(0, DaysAdapter.SelectedPayload)
+//        }
     }
 
     private fun slotClicked(position: Int, enabled: Boolean) {
@@ -163,39 +163,39 @@ class PickUpSpotFragment: BaseFragment(), PickUpSpotView, DaysAdapter.Handler{
     }
 
     override fun updateSlots(intervalSlots: List<CampaignInterval.Slot>) {
-        pickupIntervalList.removeAllViews()
-
-        val inflater = layoutInflater
-
-        if(intervalSlots.isEmpty()){
-            pickupEmpty.visibility = View.VISIBLE
-        } else{
-            pickupSv.visibility = View.VISIBLE
-        }
-
-        pickupEmpty.visibility = if(intervalSlots.isEmpty()) View.VISIBLE else View.GONE
-
-        pickupSv.visibility = if(intervalSlots.isEmpty()) View.GONE else View.VISIBLE
-
-        intervalSlots.forEachIndexed { index, interval ->
-            val view = inflater.inflate(R.layout.item_interval_campaign, pickupIntervalList, false)
-
-            val active = interval.slots > 0
-
-            view.campaignInterval.text = getString(R.string.time_range, interval.start, interval.end)
-
-            when(interval.slots){
-                0 -> { view.campaignIntervalSpots.text = getString(R.string.full) }
-                1 -> view.campaignIntervalSpots.text = getString(R.string.spot_one_format, interval.slots)
-                else -> view.campaignIntervalSpots.text = getString(R.string.spot_format, interval.slots)
-            }
-
-            updateInterval(view, active)
-
-            view.setOnClickListener {slotClicked(index, active)}
-
-            pickupIntervalList.addView(view)
-        }
+//        pickupIntervalList.removeAllViews()
+//
+//        val inflater = layoutInflater
+//
+//        if(intervalSlots.isEmpty()){
+//            pickupEmpty.visibility = View.VISIBLE
+//        } else{
+//            pickupSv.visibility = View.VISIBLE
+//        }
+//
+//        pickupEmpty.visibility = if(intervalSlots.isEmpty()) View.VISIBLE else View.GONE
+//
+//        pickupSv.visibility = if(intervalSlots.isEmpty()) View.GONE else View.VISIBLE
+//
+//        intervalSlots.forEachIndexed { index, interval ->
+//            val view = inflater.inflate(R.layout.item_interval_campaign, pickupIntervalList, false)
+//
+//            val active = interval.slots > 0
+//
+//            view.campaignInterval.text = getString(R.string.time_range, interval.start, interval.end)
+//
+//            when(interval.slots){
+//                0 -> { view.campaignIntervalSpots.text = getString(R.string.full) }
+//                1 -> view.campaignIntervalSpots.text = getString(R.string.spot_one_format, interval.slots)
+//                else -> view.campaignIntervalSpots.text = getString(R.string.spot_format, interval.slots)
+//            }
+//
+//            updateInterval(view, active)
+//
+//            view.setOnClickListener {slotClicked(index, active)}
+//
+//            pickupIntervalList.addView(view)
+//        }
     }
 
     private fun updateInterval(view: View, isActive: Boolean) {
@@ -209,13 +209,13 @@ class PickUpSpotFragment: BaseFragment(), PickUpSpotView, DaysAdapter.Handler{
     }
 
     private fun updateList(previousPosition: Int?, currentPosition: Int) {
-        previousPosition?.let {
-            val previous = pickupIntervalList.getChildAt(it)
-            changeSelected(previous, false)
-        }
-
-        val current = pickupIntervalList.getChildAt(currentPosition)
-        changeSelected(current, true)
+//        previousPosition?.let {
+//            val previous = pickupIntervalList.getChildAt(it)
+//            changeSelected(previous, false)
+//        }
+//
+//        val current = pickupIntervalList.getChildAt(currentPosition)
+//        changeSelected(current, true)
     }
 
     private fun changeSelected(view: View?, isSelected: Boolean) {
@@ -223,17 +223,17 @@ class PickUpSpotFragment: BaseFragment(), PickUpSpotView, DaysAdapter.Handler{
     }
 
     override fun showProgress() {
-        clickEnabled = false
-
-        pickupBook.visibility = View.GONE
-        pickupBookProgress.visibility = View.VISIBLE
+//        clickEnabled = false
+//
+//        pickupBook.visibility = View.GONE
+//        pickupBookProgress.visibility = View.VISIBLE
     }
 
     override fun hideProgress() {
-        pickupBookProgress.visibility = View.GONE
-        pickupBook.visibility = View.VISIBLE
-
-        clickEnabled = true
+//        pickupBookProgress.visibility = View.GONE
+//        pickupBook.visibility = View.VISIBLE
+//
+//        clickEnabled = true
     }
 
     private fun getCampaign() = arguments?.getParcelable(EXTRA_CAMPAIGN) as Campaign
